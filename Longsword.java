b87a11b4085da2437cf9faf64ab42fd37cc9fce9
Edward Joseph <ejoseph1@eagles.ewu.edu>
2020-03-03 09:58:42 -0800

    changed tostring, started driver

--- Longsword.java
+++ Longsword.java
@@ -1,14 +1,19 @@
 public class Longsword implements AttackBehavior {
 
     @Override
-    public void attack(DungeonCharacter enemy) {
+    public int attack(DungeonCharacter enemy) {
         boolean doesHit = Dice.d20() - enemy.getAC() > 0;
         if (doesHit) {
             int dam = Dice.d8();
             enemy.subtractHitPoints(dam);
-            System.out.printf("You slash the %s for %d damage!\n", enemy.getName(), dam);
+            return dam;
         } else {
-            System.out.printf("You miss the %s with your attack.\n", enemy.getName());
+            return 0;
         }
+    }
+
+    @Override
+    public String toString() {
+        return " slashes ";
     }
 }
