fd7f5fe7dfaadd49747aaf1c35055a0c00768974
nhiblar <46576774+nhiblar@users.noreply.github.com>
2020-03-10 10:03:28 -0700

    Add files via upload
    AC and HP balance adjust

--- gameCharacters/HeroFactory.java
+++ gameCharacters/HeroFactory.java
@@ -8,33 +8,33 @@ public class HeroFactory {
 
 		switch(charRace) {
 			case "Aarakocra":
-				return new Aarakocra(name, 14, 3, 16, charClass, new ShortSword());
+				return new Aarakocra(name, 16, 3, 14, charClass, new ShortSword());
 			case "Bugbear":
-				return new BugBear  (name, 19, 1, 17, charClass, new GreatSword());
+				return new BugBear  (name, 21, 1, 15, charClass, new GreatSword());
 			case "Elf":
-				return new Elf 		(name, 15, 4, 13, charClass, new LongSword());
+				return new Elf 		(name, 17, 4, 11, charClass, new LongSword());
 			case "Dragonborn":
-				return new Dragonborn(name, 16, 3, 14, charClass, new Greataxe());
+				return new Dragonborn(name, 18, 3, 12, charClass, new Greataxe());
 			case "Dwarf":
-				return new Dwarf	(name, 18, 3, 15, charClass, new Mace());
+				return new Dwarf	(name, 20, 3, 13, charClass, new Mace());
 			case "Gnome":
-				return new Gnome	(name, 13, 4, 15, charClass, new Club());
+				return new Gnome	(name, 15, 4, 12, charClass, new Club());
 			case "Goliath":
-				return new Goliath	(name, 18, 1, 18, charClass, new Greataxe());
+				return new Goliath	(name, 20, 1, 16, charClass, new Greataxe());
 			case "Halfelf":
-				return new HalfElf	(name, 16, 3, 14, charClass, new Quarterstaff());
+				return new HalfElf	(name, 18, 3, 12, charClass, new Quarterstaff());
 			case "Human":
-				return new Human	(name, 15, 3, 15, charClass, new LongSword());
+				return new Human	(name, 17, 3, 13, charClass, new LongSword());
 			case "Lizardfolk":
-				return new Lizardfolk(name, 16, 2, 14, charClass, new Mace());
+				return new Lizardfolk(name, 18, 2, 12, charClass, new Mace());
 			case "Triton":
-				return new Triton	(name, 14, 2, 16, charClass, new ShortSword());
+				return new Triton	(name, 16, 2, 14, charClass, new ShortSword());
 			case "Tabaxi":
-				return new Tabaxi	(name, 16, 3, 14, charClass, new Quarterstaff());
+				return new Tabaxi	(name, 18, 3, 12, charClass, new Quarterstaff());
 				
 		}
 		System.out.println("Defying the creator, a hero spawns from the eather.");
-		return new Human (name, 15, 3, 15, charClass, new ShortSword());
+		return new Human (name, 17, 3, 13, charClass, new ShortSword());
 	}
 	
 
